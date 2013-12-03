82bd97053b9411b8b6ff1a375de268c3081cb0a9
Julian Lamas-Rodriguez <lamas@zib.de>
2013-12-03 15:55:48 +0100

    non-local-means: clarify code

--- non-local-means/loopswapped/loopswappednml.py
+++ non-local-means/loopswapped/loopswappednml.py
@@ -223,7 +223,8 @@ def main():
     nois_img_std  = nois_img.std()
 
     normal_nois_img = nois_img - nois_img_mean
-    if nois_img_std != 0.000001: normal_nois_img /= nois_img_std
+    if nois_img_std != 0.000001: # Divide by std. dev. if it is not zero
+        normal_nois_img /= nois_img_std
 
     print "Denoising image..."
     normal_rest_img = denoise2D(normal_nois_img, True)
