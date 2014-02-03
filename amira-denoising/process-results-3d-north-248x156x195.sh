fe46f561328e64a0ef335f5939f8361315c63a89
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-03 19:39:05 +0100

    amira-denoising: add new denoising script

---
+++ amira-denoising/process-results-3d-north-248x156x195.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source process-results.sh
+
+global_variables() {
+    # Get directory with original files and directory to store the
+    # processed files
+    orig_dir=$1
+    proc_dir=$2
+
+    # Set direction
+    direction="xy"
+
+    # Set the rotations to perform in the images
+    rotation_xy=0
+    rotation_xz=0
+    rotation_yz=0
+
+    # Set the dimensions to crop the images
+    crop_dims_xy=150x150+0+0
+    crop_dims_xz=150x150+0+20
+    crop_dims_yz=150x150+0+40
+}
+
+main() {
+    global_variables $@
+    process_results
+}
+
+main $@
