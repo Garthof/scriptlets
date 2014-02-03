10c8d91f2437f588f1a13b4b950d7d12effd194c
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-03 18:48:52 +0100

    amira-denoising: add scripts to denoise new volumes

---
+++ amira-denoising/process-results-xz-east-247x213x230.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source process-results.sh
+
+global_variables() {
+    # Get directory with original files and directory to store the
+    # processed files
+    orig_dir=$1
+    proc_dir=$2
+
+    # Set direction
+    direction="xz"
+
+    # Set the rotations to perform in the images
+    rotation_xy=180
+    rotation_xz=0
+    rotation_yz=270
+
+    # Set the dimensions to crop the images
+    crop_dims_xy=210x210+1+20
+    crop_dims_xz=210x210+0+0
+    crop_dims_yz=210x210+1+20
+}
+
+main() {
+    global_variables $@
+    process_results
+}
+
+main $@
---
+++ amira-denoising/process-results-yz-north-248x156x195.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+source process-results.sh
+
+global_variables() {
+    # Get directory with original files and directory to store the
+    # processed files
+    orig_dir=$1
+    proc_dir=$2
+
+    # Set direction
+    direction="yz"
+
+    # Set the rotations to perform in the images
+    rotation_xy=270
+    rotation_xz=270
+    rotation_yz=90
+
+    # Set the dimensions to crop the images
+    crop_dims_xy=150x150+0+40
+    crop_dims_xz=150x150+0+20
+    crop_dims_yz=150x150+0+0
+}
+
+main() {
+    global_variables $@
+    process_results
+}
+
+main $@
