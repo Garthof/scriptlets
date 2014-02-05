b356ac71eb44f9bf2947209bb06cbd122abbb2a6
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-05 14:54:57 +0100

    non-local-means: read input and output files from script arguments

--- non-local-means/adaptivemanifolds/denoise.m
+++ non-local-means/adaptivemanifolds/denoise.m
@@ -1,7 +1,16 @@
 #!/usr/bin/octave -qf
 
+% Check args
+if (nargin < 2)
+    usage('%s', [program_name() ' input_file' ' output_file']);
+end
+
+% Read args
+input_file = argv(){1};
+output_file = argv(){2};
+
 % Load file
-s = load('Ammonit-Eo_u-cropped-141x152x180.vol.mat');
+s = load(input_file);
 
 for [val key] = s
 	data = val;
@@ -11,4 +20,4 @@ end
 % Normalize image?
 
 % Save file
-save('-mat-binary', 'test.mat', 'data');
+save('-mat-binary', output_file, 'data');
