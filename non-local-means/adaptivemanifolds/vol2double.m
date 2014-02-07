8f57e1cbd53775dafd00b150d734ce94be018647
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-07 14:55:44 +0100

    non-local-means: check if volume contains floats before transforming it

--- non-local-means/adaptivemanifolds/vol2double.m
+++ non-local-means/adaptivemanifolds/vol2double.m
@@ -2,6 +2,10 @@ function [output_volume min_value max_value] = vol2double(input_volume)
     min_value = min(min(min(input_volume)));
     max_value = max(max(max(input_volume)));
 
-    output_volume = double(input_volume-min_value);
-    output_volume = output_volume / double(max_value-min_value);
+    if (isa(input_volume, 'float'))
+        output_volume = input_volume;
+    else
+        output_volume = double(input_volume-min_value);
+        output_volume = output_volume / double(max_value-min_value);
+    end
 end
