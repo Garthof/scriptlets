c8666a774ba90f9f14fcc38b4b09a21ea41f5f84
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-10 18:33:15 +0100

    non-local-means: reimplement patch space scaling
    * Previous version added NA values (that is, not available data) at
      the edges of the matrix. This version seems to avoid that problem
      (I still don't know exactly how the indices should go exactly).

--- non-local-means/adaptivemanifolds/adaptive_manifold_filter.m
+++ non-local-means/adaptivemanifolds/adaptive_manifold_filter.m
@@ -212,30 +212,3 @@ end
 p = p / norm(p);
 
 end
-
-
-% Resize each component of the patch space separatedly
-function out_patch_space = patch_space_scale(in_patch_space, m, op)
-
-[h w d nn] = size(in_patch_space);
-
-for n = 1:nn
-    out_patch_space(:,:,:,n) = volscale(in_patch_space(:,:,:,n), m, op);
-end
-
-end
-
-
-% Resize a volume of data using interp3. Kudos to Martin, who pointed me to
-% a possible solution, here: http://stackoverflow.com/a/12521658/1679
-function out_volume = volscale(in_volume, m, op)
-
-[h w d] = size(in_volume);
-
-[xx yy zz] = ndgrid(linspace(1, h, m(1)), ...
-                    linspace(1, w, m(2)), ...
-                    linspace(1, d, m(3)));
-
-out_volume = interp3(in_volume, xx, yy, zz);
-
-end
