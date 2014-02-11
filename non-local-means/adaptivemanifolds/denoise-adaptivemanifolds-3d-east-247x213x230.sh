e1d07ac86aeca23f14d9aa7adf45c5e05ffcae1e
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-11 17:43:51 +0100

    non-local-means: add two new batteries of tests

---
+++ non-local-means/adaptivemanifolds/denoise-adaptivemanifolds-3d-east-247x213x230.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source denoise-adaptivemanifolds.sh
+
+global_variables() {
+    global_script_path="/vis/data/people/bzflamas/scriptlets/non-local-means/adaptivemanifolds"
+    global_script_name="denoise.m"
+
+    global_num_iters="10"
+    global_patch_radiuses="2 3 5"
+    global_pca_dims="3 5 7"
+    global_data_stddevs="0.1 0.3 0.5 1.0"
+    global_spatial_stddevs="0.5 1.0 5.0 9.0"
+    global_num_pca_iters="2"
+
+    global_volume_name="Ammonit-Eo_u-cropped-247x213x230.vol.mat"
+    global_experiment_name="denoise-adaptivemanifolds-3d-east-247x213x230"
+    global_input_path="/vis/data/people/bzflamas/AmmoniteDenoising/datasets/volumes/Ammonit-Nano-CT/matlab/east-247x213x230"
+    global_output_path="/vis/data/people/bzflamas/AmmoniteDenoising/datasets/volumes/output/$global_experiment_name"
+    global_temp_path="/tmp"
+
+    global_simulation_flag="0"
+    global_sleep_seconds="3"
+}
+
+main() {
+    set -e
+
+    global_variables
+    iterative_denoise
+}
+
+main $*
