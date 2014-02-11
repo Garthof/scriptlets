563999d3d451fc70efb90b4c5c3f42312b598dc8
Julian Lamas-Rodriguez <lamas@zib.de>
2014-02-11 16:41:04 +0100

    non-local-means: change values for standard deviations

--- non-local-means/adaptivemanifolds/denoise-adaptivemanifolds-3d-center-141x152x180.sh
+++ non-local-means/adaptivemanifolds/denoise-adaptivemanifolds-3d-center-141x152x180.sh
@@ -9,8 +9,8 @@ global_variables() {
     global_num_iters="10"
     global_patch_radiuses="2 3 5"
     global_pca_dims="3 5 7"
-    global_data_stddevs="0.6 0.8 1.0"
-    global_spatial_stddevs="0.6 0.8 1.0"
+    global_data_stddevs="0.1 0.3 0.5 1.0"
+    global_spatial_stddevs="0.5 1.0 5.0 9.0"
     global_num_pca_iters="2"
 
     global_volume_name="Ammonit-Eo_u-cropped-141x152x180.vol.mat"
