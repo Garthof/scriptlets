cb61cffd36d3ef35972020986c39bacd3154bc0a
Julian Lamas-Rodriguez <lamas@zib.de>
2014-03-15 16:32:05 +0100

    pca: add some handy functions to debug

---
+++ pca/src/cudadebug.h
@@ -0,0 +1,29 @@
+#ifndef CUDADEBUG_H
+#define CUDADEBUG_H
+
+#include <sstream>
+#include <string>
+
+template <typename T>
+inline std::string
+toString(const T &t)
+{
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
+
+
+inline void
+saveGPUBuffer(
+        const std::string fileName,
+        const int depth, const int width, const int height,
+        const thrust::device_vector<CUDAPCA::data_t> &d_data)
+{
+    const thrust::host_vector<float> h_data(d_data);
+
+    const TmpFormat::TmpData t_data(depth, width, height, 1, h_data.data());
+    TmpFormat::saveFile(fileName + toString(".tmp"), t_data);
+}
+
+#endif
